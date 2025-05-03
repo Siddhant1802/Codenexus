74999a80c77b12deae8af4f101c737ded10e20f2
Pratham Dedhiya <68095465+prathamdedhiya17@users.noreply.github.com>
2025-05-02 22:37:08 -0400

    Added Demo Files

---
+++ demoFiles/Code.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Code {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        System.out.println("You entered: " + num1 + " and " + num2);
+    }
+}
---
+++ demoFiles/demo.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func main() {
+    var num1, num2 int
+
+    fmt.Scan(&num1, &num2)
+
+    fmt.Println("You entered:", num1, "and", num2)
+}
